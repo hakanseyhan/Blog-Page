7a380cb532a5d6b60de7869b6241528435ae30fe
hakanseyhan <hknsyn22@gmail.com>
2022-09-29 15:05:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,165 @@
+// Navigation Bar
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+let val;
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  } else {
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+}
+
+//Hamburger Menu
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = function () {
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+}
+cancelBtn.onclick = function () {
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+}
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function () {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+  });
+}
+
+
+
+let root = document.querySelector(":root");
+let coloText = document.querySelector(".home-content span");
+
+let li = document.querySelectorAll(".faq-text li");
+for (var i = 0; i < li.length; i++) {
+  li[i].addEventListener("click", (e) => {
+    let clickedLi;
+    if (e.target.classList.contains("question-arrow")) {
+      clickedLi = e.target.parentElement;
+    } else {
+      clickedLi = e.target.parentElement.parentElement;
+    }
+    clickedLi.classList.toggle("showAnswer");
+  });
+}
+
+// Accordion FAQ
+const accordionContent = document.querySelectorAll(".accordion-content");
+
+accordionContent.forEach((item, index) => {
+  let header = item.querySelector("header");
+  header.addEventListener("click", () => {
+    item.classList.toggle("open");
+
+    let description = item.querySelector(".description");
+    if (item.classList.contains("open")) {
+      description.style.height = `${description.scrollHeight}px`; //scrollHeight özelliği, dolgu dahil ancak kenarlıklar, kaydırma çubuğu veya kenar boşluğu hariç bir öğenin yüksekliğini döndürür
+      item.querySelector("i").classList.replace("fa-plus", "fa-minus");
+    } else {
+      description.style.height = "0px";
+      item.querySelector("i").classList.replace("fa-minus", "fa-plus");
+    }
+    removeOpen(index); //işlevi çağırmak ve ayrıca tıklanan başlığın dizin numarasını geçmek
+  })
+})
+function removeOpen(index1) {
+  accordionContent.forEach((item2, index2) => {
+    if (index1 != index2) {
+      item2.classList.remove("open");
+
+      let des = item2.querySelector(".description");
+      des.style.height = "0px";
+      item2.querySelector("i").classList.replace("fa-minus", "fa-plus");
+    }
+  })
+}
+
+//Giriş Formu
+const form = document.querySelector("form"),
+  emailField = form.querySelector(".email-field"),
+  emailInput = emailField.querySelector(".email"),
+  passField = form.querySelector(".create-password"),
+  passInput = passField.querySelector(".password"),
+  cPassField = form.querySelector(".confirm-password"),
+  cPassInput = cPassField.querySelector(".cPassword");
+
+
+// E-posta Doğrulama
+function checkEmail() {
+  const emaiPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  if (!emailInput.value.match(emaiPattern)) {
+    return emailField.classList.add("invalid"); //e-posta değeri e-posta kalıbıyla eşleşmezse geçersiz sınıf ekleme
+  }
+  emailField.classList.remove("invalid"); //e-posta değeri emaiPattern ile eşleşirse geçersiz sınıfın kaldırılması
+}
+
+// Şifreyi gizle ve göster
+const eyeIcons = document.querySelectorAll(".show-hide");
+
+eyeIcons.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    const pInput = eyeIcon.parentElement.querySelector("input"); //göz simgesinin ana öğesini alma ve şifre girişini seçme
+    if (pInput.type === "password") {
+      eyeIcon.classList.replace("bx-hide", "bx-show");
+      return (pInput.type = "text");
+    }
+    eyeIcon.classList.replace("bx-show", "bx-hide");
+    pInput.type = "password";
+  });
+});
+
+// Şifre Doğrulama
+function createPass() {
+  const passPattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  if (!passInput.value.match(passPattern)) {
+    return passField.classList.add("invalid"); //parola giriş değeri passPattern ile eşleşmiyorsa geçersiz sınıf ekleme
+  }
+  passField.classList.remove("invalid"); // parola giriş değeri passPattern ile eşleşirse geçersiz sınıfın kaldırılması
+}
+
+// Parola Doğrulamayı Onaylayın
+function confirmPass() {
+  if (passInput.value !== cPassInput.value || cPassInput.value === "") {
+    return cPassField.classList.add("invalid");
+  }
+  cPassField.classList.remove("invalid");
+}
+
+// Form Özetinde Fonksiyon Çağırma
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); //form göndermeyi engelleme
+  checkEmail();
+  createPass();
+  confirmPass();
+
+  //tuşa basıldığında arama işlevi
+  emailInput.addEventListener("keyup", checkEmail);
+  passInput.addEventListener("keyup", createPass);
+  cPassInput.addEventListener("keyup", confirmPass);
+
+  if (
+    !emailField.classList.contains("invalid") &&
+    !passField.classList.contains("invalid") &&
+    !cPassField.classList.contains("invalid")
+  ) {
+    location.href = form.getAttribute("action");
+  }
+});
